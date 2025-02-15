ac5710166a8d355ff337fab187cace62e744603b
berylann <berylaann@gmail.com>
2025-02-15 10:53:04 +0000

    fisrt commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Prefooter from "./components/prefooter/Prefooter";
+import WhyNiyo from "./components/whyniyo/WhyNiyo";
+
+
+function App() {
+  return (
+    <div>
+      <WhyNiyo/>
+      <Prefooter/>
+    </div>
+  );
+}
+
+export default App;
